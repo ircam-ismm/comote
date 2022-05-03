101052d9773e3d287cd8cc524bc5ccac9c1f4de4
Jean-Philippe Lambert <Jean-Philippe.Lambert@ircam.fr>
2022-05-03 23:40:07 +0900

    Sensors: normalise accelerometer for Android and iOS

--- components/SensorsComponent.tsx
+++ components/SensorsComponent.tsx
@@ -70,7 +70,7 @@ export default function SensorsComponent({color}) {
         dispatch({
           type: 'sensors/set',
           payload: {
-            accelerometer,
+            accelerometer: normalisedAccelerometer,
           },
         });
       })
