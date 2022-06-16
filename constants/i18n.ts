103c4485eefc777b60ac6319d6dfd42cfb1948d8
Riccardo Borghesi <borghesi@touriste-147-36.ircam.fr>
2022-06-16 11:27:27 +0200

    added italian support

--- constants/i18n.ts
+++ constants/i18n.ts
@@ -115,4 +115,62 @@ export default {
       privacyPolicyLink: 'https://ismm-apps.ircam.fr/comote-privacy-policy-fr',
     },
   },
+  it: {
+    nav: {
+      home: `Pagina Iniziale`,
+      play: 'Esegui',
+      settings: 'Impostazioni',
+      qrcode: 'QR Code',
+      about: 'Informazioni',
+    },
+    home: {
+      play: 'Esegui',
+      settings: 'Impostazioni',
+      qrcode: 'QR Code',
+    },
+    play: {
+      header: 'Esegui',
+      a: 'A',
+      b: 'B',
+      lock: `Blocca lo schermo (premere a lungo...)`,
+      unlock: `Sblocca lo schermo (premere a lungo...)`,
+    },
+    settings: {
+      header: 'Impostazioni',
+      scanQrCode: 'Configura da QR Code',
+      general: {
+        header: 'Generale',
+        id: 'Id',
+        period: 'Periodo',
+      },
+      websocket: {
+        header: 'WebSocket',
+        activate: 'Attiva',
+        status: 'Stato',
+        url: 'URL',
+        urlPlaceholder: 'ws://websocket.server.ip:port',
+      },
+      osc: {
+        header: 'OSC',
+        activate: 'Attiva',
+        status: 'Stato',
+        url: 'URL',
+        urlPlaceholder: 'udp://osc.server.ip:port',
+      },
+    },
+    qrcode: {
+      header: 'Scansiona QR Code',
+      requestingPermission: `Richiesta d'autorizzazione per la fotocamera...`,
+      noPermission: `Permesso di accesso alla fotocamera negato.`,
+      waitingCamera: `In attesa della fotocamera...`,
+      openSettings: `Apri le impostazioni dell'applicazione`,
+    },
+    about: {
+      header: `Informazioni su CoMo.te`,
+      developedBy: `CoMo.te è sviluppato dall'Ircam e dall'unità mista di ricerca STMS, che è sostenuta dall'Ircam, dal CNRS, dal Ministero della Cultura Francese e dall'Università della Sorbonne.`,
+      producedBy: `Realizzato con il sostegno del dispositivo Édu-up del Ministero dell'Educazione Nazionale, dell'Agenzia Nazionale della Ricerca (progetto ELEMENT), e in collaborazione con Radio France.`,
+      privacyPolicy: 'Politica di confidenzialità',
+      privacyPolicyLink: 'https://ismm-apps.ircam.fr/comote-privacy-policy-fr',
+    },
+  },
 }
