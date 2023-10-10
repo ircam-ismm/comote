2457168a6ca13f75154603747939de0640838ed2
jipodine <jipodine@users.noreply.github.com>
2023-10-10 14:38:11 +0200

    Chore: Metro: do not log config

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-console.log('config', config);
-
 // expo (49) over-rides metro's default
 // i18n-js needs '.mjs'
 config.watcher.additionalExts.push('mjs', 'cjs');
